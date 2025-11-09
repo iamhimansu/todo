360cae7e59fcb58ad56708006656c4bfcab976e4
Himanshu Raj Aman <iamhimanshu7102@gmail.com>
2025-11-09 21:55:17 +0530

    Added List items and dummy todos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'; // We'll use this to make the API call
-function App() {
-  const [message, setMessage] = useState('');
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+  ListItemIcon,
+  Checkbox,
+  IconButton,
 
+} from '@mui/material';
+import CommentIcon from '@mui/icons-material/Comment';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [checked, setChecked] = React.useState([0]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
   useEffect(() => {
     axios.get('http://localhost:5001/api/test')
       .then(response => {
-        setMessage(response.data.message);
+        setTodos(response.data);
       })
       .catch(error => {
         console.error('There was an error fetching the data!', error);
@@ -16,11 +41,39 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Your Full-Stack App</h1>
+        <h1>Welcome to Your Todo App</h1>
 
-        <p>
-          <strong>Message from backend:</strong> {message}
-        </p>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          {todos.map((todo) => {
+            const labelId = `checkbox-list-label-${todo}`;
+
+            return (
+              <ListItem
+                key={todo.id}
+                secondaryAction={
+                  <IconButton edge="end" aria-label="comments">
+                    <CommentIcon />
+                  </IconButton>
+                }
+                disablePadding
+              >
+                <ListItemButton role={undefined} onClick={handleToggle(todo.id)} dense>
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={checked.includes(todo.id)}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText id={labelId} primary={todo.text} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+
       </header>
     </div>
   );
